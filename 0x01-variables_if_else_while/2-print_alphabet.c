5d58f1dd4f6dc0c53a9e634eb9a097348d3ffb8d
Kiruben <kiruben.dev@gmail.com>
2022-09-09 11:20:18 -0700

    2-print_alphabet.c 1.1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,18 +4,15 @@
  *
  *Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-  char ch;
-  ch = 'a';
-
-  while (ch <= 'z')
-    {
-      putchar(ch);
-      ch++;
-    }
-
-  putchar('\n');
-
-  return (0);
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
 }
