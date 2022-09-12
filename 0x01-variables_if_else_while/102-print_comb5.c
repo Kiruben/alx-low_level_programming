ada7022f5633f1c941ea6ad75b82d55a9807c5e4
Kiruben <kiruben.dev@gmail.com>
2022-09-12 06:23:05 -0700

    102-print_comb5.c 1.0

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *description - prints all possible combos of two
+ *two-digit numbers
+ *return - always 0 (success)
+ */
+int main(void)
+{
+int first_num, second_num;
+first_num = 0;
+while (first_num <= 98)
+{
+second_num = first_num + 1;
+while (second_num <= 99)
+{
+putchar((first_num / 10) + '0');
+putchar((first_num % 10) + '0');
+putchar(' ');
+putchar((second_num / 10) + '0');
+putchar((second_num % 10) + '0');
+if (first_num == 98 && second_num == 99)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+second_num++;
+}
+first_num++;
+}
+return (0);
+}
