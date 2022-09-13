e0bc34ed0b8687d553ff72c236df9e4e372cccc2
Kiruben <kiruben.dev@gmail.com>
2022-09-13 11:12:18 -0700

    2-alphabet_x10.c 1.0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prototype funtcion
+ *Description - prints alphabet 10x in lowercase
+ *Return - none
+ */
+void print_alphabet_x10(void)
+{
+int c;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
+}
