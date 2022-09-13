cae413e73acbdaa0c03999c27648fe6b2efc5449
Kiruben <kiruben.dev@gmail.com>
2022-09-13 12:00:51 -0700

    6-abs.c 1.0

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ *_abs - prototype function
+ *@num: number to evaluate
+ *Decription: Prints absolute value of number
+ *Return: returns absolute value of number
+ */
+int _abs(int num)
+{
+if (num < 0)
+return (-1 * num);
+return (num);
+}
