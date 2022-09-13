c47012534551ad8f6132155a5a5fed04a86eda93
Kiruben <kiruben.dev@gmail.com>
2022-09-13 11:03:11 -0700

    1-alphabet.c 1.0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prototype function
+ *Description - Prints alphabet in lowercase
+ *Return: none
+ */
+void print_alphabet(void)
+{
+int i;
+for(i = 'a'; i <= 'z' ; i++)
+_putchar(i);
+_putchar('\n');
+}
