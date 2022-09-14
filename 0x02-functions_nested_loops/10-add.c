15cf457e8b543a483cb94b0dd4cd5a1b21a24150
Kiruben <kiruben.dev@gmail.com>
2022-09-14 02:00:08 -0700

    10-add.c 1.0

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ *add - prototype function
+ *@num_one: first number to be added
+ *@num_two: second number to be added
+ *Description: function that adds two integers
+ *Return: returns sum of integers
+ */
+int add(int num_one, int num_two)
+{
+return (num_one + num_two);
+}
