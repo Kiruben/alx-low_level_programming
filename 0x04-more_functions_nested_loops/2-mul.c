cd7502393734a1a4ea8a65e1a844adb70c505194
Kiruben <kiruben.dev@gmail.com>
2022-09-15 11:33:06 -0700

    2-mul.c 1.0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ *mul - prototype function
+ *@a: first number to multiply by
+ *@b: second number to multiply by
+ *Description: function to multiply two integers
+ *Return: return multiple of two intergers
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
