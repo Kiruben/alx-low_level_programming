249a8cd4d730c2a7e456b06fe820e995909257ec
Kiruben <kiruben.dev@gmail.com>
2022-09-15 11:17:57 -0700

    1-isdigit.c 1.0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ *_isdigit - prototype function
+ *@c: number to be evaluated
+ *Description - check to see if c is a digit
+ *Return: 1 if c is a digit 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
