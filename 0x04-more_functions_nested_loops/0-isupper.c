89d4efc286dbd28e1cdaf7fe565cccb3a3849448
Kiruben <kiruben.dev@gmail.com>
2022-09-15 11:07:58 -0700

    0-isupper.c 1.0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+/**
+ *_isupper - protoype function
+ *@c: character to check if uppercase
+ *Description: checks to see if c is uppercase
+ *Return: 1 if uppercaseor 0 is not
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
