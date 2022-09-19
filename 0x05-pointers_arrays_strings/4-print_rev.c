c9464eb617affb44ca6de67456e720974de9676b
Kiruben <kiruben.dev@gmail.com>
2022-09-19 09:44:29 -0700

    4-print_rev.c 1.0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_rev - prototype function
+ *@s: string to print in reverse
+ *Decription: prints string in reverse
+ *Return: void
+ */
+void print_rev(char *s)
+{
+int size;
+size = 0;
+while (*(s + size) != '\0')
+{
+size++;
+}
+while (size >= 0)
+{
+_putchar(*(s + size));
+size--;
+}
+_putchar('\n');
+}
