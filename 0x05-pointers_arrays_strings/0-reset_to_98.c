7f92f7486a63076b344312edbce310facef803b0
Kiruben <kiruben.dev@gmail.com>
2022-09-19 08:34:27 -0700

    0-reset_to_98.c 1.0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ *reset_to_98 - prototype function
+ *@n: pointer to value needed to update
+ *Description: updates the value n points to
+ *Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
