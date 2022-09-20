104e256e9d611836ea6ee92abf8c6084c048ab61
Kiruben <kiruben.dev@gmail.com>
2022-09-20 07:32:49 -0700

    8-print_array.c 1.0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,17 @@
  */
 void print_array(int *a, int n)
 {
-  int i;
-  for(i=0;i<n;i++)
-    {
-      if((i + 1) != n)
-	{
-      printf("%d, ",a[i]);
-	}
-      else
-	{
-	  printf("%d",a[i]);
-	}
-    }
- putchar('\n');
+int i;
+for (i = 0; i < n; i++)
+{
+if ((i + 1) != n)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d", a[i]);
+}
+}
+putchar('\n');
 }
