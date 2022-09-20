3c5f9a402c2a90d954852b6db71e1c22dfb5ed38
Kiruben <kiruben.dev@gmail.com>
2022-09-20 06:58:51 -0700

    7-puts_half.c 1.0

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *puts_half - prototype function
+ *@s: string to be evaluated
+ *Description: prints half of the string
+ *Return: void
+ */
+void puts_half(char *s)
+{
+int size;
+int half_size;
+size = 0;
+while (s[size] != '\0')
+{
+size++;
+}
+if ((size - 1) % 2 == 0)
+{
+half_size = 0;
+size = size / 2;
+while (half_size < size)
+{
+_putchar(s[half_size]);
+half_size++;
+}
+}
+else
+{
+half_size = size / 2;
+while (half_size < size)
+{
+_putchar(s[half_size]);
+half_size++;
+}
+}
+_putchar('\n');
+}
