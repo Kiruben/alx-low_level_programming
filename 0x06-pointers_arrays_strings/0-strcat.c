ece1b01da56b900c9ad1d56935bbf14c3356ec51
Kiruben <kiruben.dev@gmail.com>
2022-09-21 06:16:30 -0700

    0-strcat.c 1.0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strcat - prototype function
+ *@dest: string src needs to be appended to
+ *@src: string that needs to be appended to dest
+ *Description: concatenates two strings
+ *Return: dest - concatenated strings
+ */
+char *_strcat(char *dest, char *src)
+{
+int dest_size;
+int src_size;
+int i;
+dest_size = 0;
+src_size = 0;
+while (dest[dest_size] != '\0')
+{
+dest_size++;
+}
+while (src[src_size] != '\0')
+{
+src_size++;
+}
+for (i = 0; i < src_size && src[i] != '\0'; i++)
+{
+dest[dest_size + i] = src[i];
+}
+dest[dest_size + i] = '\0';
+return (dest);
+}
