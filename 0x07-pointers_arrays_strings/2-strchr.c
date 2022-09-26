3f6f6f952098313c7472bd895f66779c7d11a34d
Kiruben <kiruben.dev@gmail.com>
2022-09-26 12:40:53 -0700

    2-strchr.c 1.0

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+ * _strchr - prototype function
+ * @s: string to search
+ * @c: character to locate 
+ *Description: locates a character in a string
+ * Return: returns pointer to c position
+ */
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (c == *s)
+{
+return (s);
+}
+s++;
+}
+return ("NULL");
+}
