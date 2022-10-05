071370816894440847e2504cf740864906ad1a2d
Kiruben <kiruben.dev@gmail.com>
2022-10-05 14:40:14 -0700

    2-str_concat.c 1.1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * str_concat - prototype function 
+ * str_concat - prototype function
  *Description: A function that concatenates two strings
  * @s1: An input pointer of the first string
  * @s2: An input pointer of the second string
@@ -14,9 +14,7 @@ int count = 0, size_s1 = 0, size_s2 = 0;
 temp_s1 = s1;
 temp_s2 = s2;
 if (s1 == NULL)
-{
 s1 = "";
-}
 while (*s1)
 {
 size_s1++;
@@ -24,9 +22,7 @@ s1++;
 }
 s1 = temp_s1;
 if (s2 == NULL)
-{
 s2 = "";
-}
 while (*s2)
 {
 size_s2++;
@@ -36,9 +32,7 @@ s2 = temp_s2;
 new_str = malloc(sizeof(char) * (size_s1 + size_s2 + 1));
 temp_s1 = new_str;
 if (new_str == NULL)
-{
 return (NULL);
-}
 for (; count < (size_s1 + size_s2); count++)
 {
 if (count < size_s1)
