98e2e96e042a9009a2c04af7affea44bc6da7bd9
Kiruben <kiruben.dev@gmail.com>
2022-10-18 06:24:27 -0700

    update 2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
+#include <stddef.h>
 int _putchar(char);
-void print_name(char *, void (*)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *, void(*)(char *));
 int int_index(int *, int, int (*)(int));
 
 #endif
