07c526f3a177fd9a0075d799fa020280e16077f5
Kiruben <kiruben.dev@gmail.com>
2022-10-21 08:08:55 -0700

    1-list_len.c 1.0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - prototype function
+ *Description: counts number of elements in list
+ *@h: list to count
+ * Return: number of elements.
+ */
+size_t list_len(const list_t *h)
+{
+int count = 0;
+while(h != NULL)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
