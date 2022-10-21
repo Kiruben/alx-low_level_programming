b60d841de6388d0532de7ec07b078609c93dc7dc
Kiruben <kiruben.dev@gmail.com>
2022-10-21 11:38:16 -0700

    2-add_node.c 1.0

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,12 +11,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 char *temp_str = strdup(str);
 list_t *ptr = malloc(sizeof(list_t));
+unsigned int count = 0;
 if (ptr == NULL)
 {
 return (NULL);
 }
+while (str[count])
+count++;
 ptr->str = temp_str;
-ptr->len = strlen(temp_str);
+ptr->len = count;
 ptr->next = *head;
 *head = ptr;
 return (*head);
