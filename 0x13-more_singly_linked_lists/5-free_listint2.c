d282a435d1553fb56c707a366289a4aa2b976417
Kiruben <kiruben.dev@gmail.com>
2022-10-26 11:43:41 -0700

    5-free_listint2.c 1.5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,11 @@ void free_listint2(listint_t **head)
 listint_t *temp = *head;
 if (head == NULL)
 return;
-while (temp != NULL)
+while (*head)
 {
-temp = temp->next;
-free(*head);
-*head = temp;
+temp = *head;
+(*head) = (*head)->next;
+free(temp);
 }
 *head = NULL;
 }
