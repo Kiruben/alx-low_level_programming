4a1e7350d73758e792580f5955ac08ffbb51b3ee
Kiruben <kiruben.dev@gmail.com>
2022-10-26 11:19:09 -0700

    9-insert_nodeint.c 1.2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,15 +13,11 @@ if (head == NULL)
 {
 return (NULL);
 }
-else
-{
 listint_t *new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
 {
 return (NULL);
 }
-else
-{
 listint_t *temp_head = *head;
 unsigned int count = 0;
 new_node->n = n;
@@ -37,8 +33,6 @@ new_node->next = *head;
 *head = new_node;
 return (new_node);
 }
-else
-{
 while (temp_head)
 {
 if (count == (idx - 1))
@@ -52,6 +46,5 @@ temp_head = temp_head->next;
 }
 }
 return (NULL);
-}
 }
 }
