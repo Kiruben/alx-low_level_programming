61c4ac253c9df7e2ec6a8c4b3f4f2c954e6fde86
Kiruben <kiruben.dev@gmail.com>
2022-11-01 22:43:21 -0700

    1-print_numbers.c 1.1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ va_list ap;
 va_start(ap, n);
 for (i = 0; i < n; i++)
 {
-printf("%d ", va_arg(ap, int));
+printf("%d", va_arg(ap, int));
 }
 va_end(ap);
 printf("\n");
@@ -33,7 +33,7 @@ for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(ap, int));
 if (i != (n - 1))
-printf("%c ", *separator);
+printf("%s", separator);
 }
 va_end(ap);
 printf("\n");
