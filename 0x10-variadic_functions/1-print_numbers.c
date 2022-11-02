4b1920b2486b1d0b011ec1074131f78049061e59
Kiruben <kiruben.dev@gmail.com>
2022-11-01 22:38:28 -0700

    1-print_numbers.c 1.0

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers - prototype function
+ *Description: prints numbers from list
+ *@separator: string to prints between numbers
+ *@n: numbers of ints passed
+ *Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+if (n == 0)
+return;
+else
+{
+if (separator == NULL)
+{
+unsigned int i;
+va_list ap;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+{
+printf("%d ", va_arg(ap, int));
+}
+va_end(ap);
+printf("\n");
+}
+else
+{
+unsigned int i;
+va_list ap;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ap, int));
+if (i != (n - 1))
+printf("%c ", *separator);
+}
+va_end(ap);
+printf("\n");
+}
+}
+}
